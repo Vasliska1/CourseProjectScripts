86af5a6401ea9df32daa3be01954338656c57972
Vasliska1 <v.lisitsina@gmail.com>
2021-11-10 16:42:41 +0300

    Clean code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         int a;
         int b;
+        int c;
         Main generator = new Main();
         generator.generatorHuman();
         generator.generatorCity();
